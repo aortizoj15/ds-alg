b528860ce9ae2ff86e50e916d4e3e9fa3eb44089
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-03-03 22:35:49 -0800

    Added optimized solution for container with most water

--- 11containerWithMostWater.js
+++ 11containerWithMostWater.js
@@ -22,7 +22,17 @@
 
 // Brute Force Solution
 
-const maxArea = (nums) => {
+// Initialize max
+// Use two pointers, a and b
+// Initialize pointer a at 0 to be used by outer for loop
+// Initialize pointer b at a + 1 to be used by inner for loop
+// Calculate area using values at pointers
+// Use Math.max to store the higher value between the calculated area and the current max area
+// Once pointer b reaches the end of input array, a will move forward
+// Repeat until a reaches end of input array
+// Return max
+
+const containerWithMostWater = (nums) => {
   let max = 0;
   for (let a = 0; a < nums.length; a++) {
     for (let b = a + 1; b < nums.length; b++) {
@@ -35,14 +45,75 @@ const maxArea = (nums) => {
   return max;
 };
 
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
 // Checking Examples
 console.log(
-  "maxArea([1,8,6,2,5,4,8,3,7]) should be 49, result is: ",
-  maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+  "containerWithMostWater([1,8,6,2,5,4,8,3,7]) should be 49, result is: ",
+  containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7])
 ); // 49
-console.log("maxArea([1,1]) should be 1, result is: ", maxArea([1, 1])); // 1
 console.log(
-  "maxArea([4,3,2,1,4]) should be 16, result is: ",
-  maxArea([4, 3, 2, 1, 4])
+  "containerWithMostWater([1,1]) should be 1, result is: ",
+  containerWithMostWater([1, 1])
+); // 1
+console.log(
+  "containerWithMostWater([4,3,2,1,4]) should be 16, result is: ",
+  containerWithMostWater([4, 3, 2, 1, 4])
 ); // 16
-console.log("maxArea([1,2,1]) should be 2, result is: ", maxArea([1, 2, 1])); // 2
+console.log(
+  "containerWithMostWater([1,2,1]) should be 2, result is: ",
+  containerWithMostWater([1, 2, 1])
+); // 2
+
+// Optimized Solution
+
+// Initialize variable max which will hold the maximum area
+// Initialize pointer a at 0 (beginning) and pointer b at nums.length - 1 (end)
+// Calculate the area
+// Use Math.max to store the higher value between the calculated area and the current max area
+// Compare the values in the array at pointer a and b
+// If pointer a is bigger we move b to the left
+// If pointer b is bigger we move a to the right
+// This is to ensure the height of our next calculation is greater than or equal to the height in previous calculation
+// Repeat until pointers cross each other, while a < b
+// Return max area
+
+const containerWithMostWaterOptimized = (nums) => {
+  let max = 0;
+  let a = 0;
+  let b = nums.length - 1;
+  while (a < b) {
+    const height = Math.min(nums[a], nums[b]);
+    const width = b - a;
+    const area = height * width;
+    max = Math.max(area, max);
+    if (nums[a] <= nums[b]) {
+      a++;
+    } else {
+      b--;
+    }
+  }
+  return max;
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Checking Examples
+console.log(
+  "containerWithMostWaterOptimized([1,8,6,2,5,4,8,3,7]) should be 49, result is: ",
+  containerWithMostWaterOptimized([1, 8, 6, 2, 5, 4, 8, 3, 7])
+); // 49
+console.log(
+  "containerWithMostWaterOptimized([1,1]) should be 1, result is: ",
+  containerWithMostWaterOptimized([1, 1])
+); // 1
+console.log(
+  "containerWithMostWaterOptimized([4,3,2,1,4]) should be 16, result is: ",
+  containerWithMostWaterOptimized([4, 3, 2, 1, 4])
+); // 16
+console.log(
+  "containerWithMostWaterOptimized([1,2,1]) should be 2, result is: ",
+  containerWithMostWaterOptimized([1, 2, 1])
+); // 2
